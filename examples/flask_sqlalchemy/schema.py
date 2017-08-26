4351a3b0e35bccbda128fb3f2c8a80bceefb7eff
Vladimir E <VVErmolenko@domclick.ru>
2017-08-26 13:52:09 +0300

    example

--- examples/flask_sqlalchemy/schema.py
+++ examples/flask_sqlalchemy/schema.py
@@ -10,15 +10,8 @@ class Department(SQLAlchemyObjectType):
 
     class Meta:
         model = DepartmentModel
+        only_fields = ('name', )
         interfaces = (relay.Node, )
-
-
-class Employee(SQLAlchemyObjectType):
-
-    class Meta:
-        model = EmployeeModel
-        interfaces = (relay.Node, )
-
 
 class Role(SQLAlchemyObjectType):
 
@@ -27,11 +20,19 @@ class Role(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
+class Employee(SQLAlchemyObjectType):
+
+    class Meta:
+        model = EmployeeModel
+        only_fields = ('name', 'department', )  # careful limitations review required
+        interfaces = (relay.Node, )
+
+
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    all_employees = SQLAlchemyConnectionField(Employee)
-    all_roles = SQLAlchemyConnectionField(Role)
+    allEmployees = SQLAlchemyConnectionField(Employee)
+    allRoles = SQLAlchemyConnectionField(Role)
     role = graphene.Field(Role)
+    node = relay.Node.Field()
 
 
 schema = graphene.Schema(query=Query, types=[Department, Employee, Role])
